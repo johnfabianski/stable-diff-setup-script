a0d8eea8fabad84f2ca2621812c8302d678ec92e
samlhuillier <sam.jansa123@gmail.com>
2022-10-04 13:01:15 -0700

    updated

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-mkdir stable-diffusion/checkpoints
+mkdir ~/stable-diffusion/checkpoints
 conda init bash
 conda init zsh
-conda env create -f stable-diffusion/environment.yaml
-curl https://www.googleapis.com/storage/v1/b/aai-blog-files/o/sd-v1-4.ckpt?alt=media > stable-diffusion/checkpoints/sd-v1-4.ckpt
+conda env create -f ~/stable-diffusion/environment.yaml
+curl https://www.googleapis.com/storage/v1/b/aai-blog-files/o/sd-v1-4.ckpt?alt=media > ~/stable-diffusion/checkpoints/sd-v1-4.ckpt
